87cc163cd28c1ccbe6181f76b507ffbadfc83688
Brendan C. Ward <bcward@astutespruce.com>
2022-06-10 15:46:16 -0700

    Fix typos in build_pixelfilter

--- scripts/build_pixelfilter.sh
+++ scripts/build_pixelfilter.sh
@@ -14,7 +14,7 @@ echo "Deploying UI to $STATIC_DIR/pixelfilter"
 if docker-compose -f ./docker/ui/docker-compose.yml run --rm --user app pixelfilter-demo-build npm run deploy; then
     echo "====> UI build succeeded"
     rm -rf $STATIC_DIR/pixelfilter/*
-    cp -r $SE_CODE_DIR/ui/public/* $STATIC_DIR/pixelfilter
+    cp -r $PIXELFILTER_CODE_DIR/public/* $STATIC_DIR/pixelfilter
 else
     echo "====> ERROR: UI build failed"
 fi
