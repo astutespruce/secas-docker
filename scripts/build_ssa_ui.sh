21edf0003f2457d47021797aa72d744078f07675
Brendan C. Ward <bcward@astutespruce.com>
2022-10-20 12:38:37 -0700

    Fix bug in SSA UI build script

--- scripts/build_ssa_ui.sh
+++ scripts/build_ssa_ui.sh
@@ -14,7 +14,7 @@ echo "Deploying UI to $STATIC_DIR/ssa"
 if docker-compose -f ./docker/ui/docker-compose.yml run --rm --user app ssa-ui-build npm run deploy; then
     echo "====> UI build succeeded"
     rm -rf $STATIC_DIR/ssa/*
-    cp -r $SA_CODE_DIR/ui/public/* $STATIC_DIR/ssa
+    cp -r $SSA_CODE_DIR/ui/public/* $STATIC_DIR/ssa
 else
     echo "====> ERROR: UI build failed"
 fi
