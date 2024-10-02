78d82b7c6eae694d40359f01ec1bb39d5347967e
Brendan C. Ward <bcward@astutespruce.com>
2024-10-02 12:42:50 -0700

    Update API / UI Docker containers, cleanup python deps

--- docker/api/README.md
+++ docker/api/README.md
@@ -13,3 +13,6 @@ All share the same Python dependencies.
 
 Note: two applications use different font stacks, so both sets of fonts are
 installed into this image.
+
+Dependencies are managed using `uv` by each of the projects. See `Developing.md`
+in each project for instructions on how to sync those dependencies here.
--- docker/api/run_xvfb.sh
+++ docker/api/run_xvfb.sh
@@ -5,7 +5,9 @@ rm -f /tmp/.X99-lock
 
 # Start Xvfb
 echo "Starting Xvfb"
-Xvfb ${DISPLAY} -screen 0 1024x768x24 -ac +render -noreset -nolisten tcp  &
+# XVFB is now emitting XKEYBOARD errors which require interaction, send them to
+# null then send XVFB to background
+Xvfb ${DISPLAY} -screen 0 1024x768x24 -ac +render -noreset -nolisten tcp > /dev/null 2>&1 &
 Xvfb_pid="$!"
 echo "Waiting for Xvfb (PID: $Xvfb_pid) to be ready..."
 while ! xdpyinfo -display ${DISPLAY} > /dev/null 2>&1; do
